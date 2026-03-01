cb59fd6ee5d9076915f0e582bd6fe8ee64b8ba71
chronark <dev@chronark.com>
2026-03-01 16:45:52 +0100

    fix: leadere

--- home/.config/nvim/lua/mappings.lua
+++ home/.config/nvim/lua/mappings.lua
@@ -7,5 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "gd", vim.lsp.buf.definition, { desc = "LSP go to definition" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
